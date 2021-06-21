310f31621bb176d7ded2bc6585e8161cff341f39
Tay Nishimura <tay.nishimura@datadoghq.com>
2021-06-21 17:02:02 -0400

    inputs

--- small/args.go
+++ small/args.go
@@ -3,14 +3,24 @@ package main
 import (
     "os"
     "fmt"
+    "strings"
 )
 
 func main() {
+
+ /* FOR LOOP
     var s, separator string
     for i := 1; i < len(os.Args); i++ {
         s += separator + os.Args[i]
         separator = " & "
     }
     fmt.Println(s)
+  */
+
+    fmt.Println(strings.Join(os.Args[1:], " & "))
+
+ /* QUICK DEBUG
+    fmt.Println(os.Args[1:])
+  */
 }
 
---
+++ small/filein.go
@@ -0,0 +1,44 @@
+package main
+
+import(
+    "bufio"
+    "os"
+    "fmt"
+    "log"
+    "strings"
+)
+
+func main() {
+    counts := make(map[string]int)
+    files := os.Args[1:]
+    if len(files) == 0 {
+        log.Fatal("no files provided")
+    }
+
+    for _, filename := range files {
+        f, err := os.Open("sample_files/" + filename)
+        if err != nil {
+            fmt.Println("bad filename, sample_files/" + filename)
+            continue
+        }
+        countLines(f, counts)
+        f.Close()
+    }
+
+    for word, n := range counts {
+        if n > 1 {
+            fmt.Printf("%d\t%s\n", n, word)
+        }
+    }
+}
+
+func countLines(f *os.File, counts map[string]int) {
+    input := bufio.NewScanner(f)
+    for input.Scan() {
+        tokens := strings.Split(input.Text(), " ")
+        for _, tkn := range tokens {
+            counts[tkn]++
+        }
+    }
+} 
+
